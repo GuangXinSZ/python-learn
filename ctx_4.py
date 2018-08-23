e0dca213c6f7a318b169dddf03ac1a96611b2451
xin <1446854524@qq.com>
2018-08-24 00:13:33 +0800

    循环

---
+++ ctx_4.py
@@ -0,0 +1,11 @@
+print('please input a')
+a = input()
+a = int(a)
+if a == 1 :
+    print('apple')
+elif a == 2 :
+    print('orange')
+elif a == 3: 
+    print('banana')
+else :
+    print('shopping')
---
+++ ctx_5.py
@@ -0,0 +1,22 @@
+# 循环
+
+# 循环语句
+
+# while for
+counter = 1
+while counter < 2 :
+    counter +=1
+    print(counter)
+else :
+    print('读取完成')
+
+# for循环
+a = [['apple', 'orange', 'banana'], (1, 2, 3)]
+for x in a :
+    for y in x :
+        print(y)
+b = [1, 2, 3]
+for x in b :
+    if x == 2 :
+        break
+    print(x)
