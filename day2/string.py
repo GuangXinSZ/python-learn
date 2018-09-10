473944197359d82632f0c254da751de7e0cfa681
author <18379177354@163.com>
2018-09-10 22:58:54 +0800

    feature: py基本语法

---
+++ day2/string.py
@@ -0,0 +1,25 @@
+
+name = "my \t name is {name} and i am {year} old"
+
+# print(name.capitalize()) # 首字母大写
+# print(name.count("a")) # 索引所在位置
+# print(name.center(40, "-")) # 打印结果------my name is alex------------
+# print(name.endswith("ex")) # 结尾是不是ex ? true : false
+# print(name.expandtabs(tabsize=30)) # 加 \t 添加空格
+# print(name.find("y")) # 返回下标的索引
+# print(name[name.find("name")])
+# print(name.format(name='alex', year=33))
+# print(name.format_map( {'name': 'alex', 'year': 12} ))
+# print('abs123'.isalnum()) # 是否是一个阿拉伯数字
+# print('abA'.isalpha()) # 是否全英文
+# print('ac'.isdigit()) # 是不是一个int
+# print('aa'.isidentifier()) # 判断是不是一个合法的标识符
+# print('My Name'.istitle())
+# print('my'.isupper()) # 是否是全部大写
+# print('My Name Is'.join('==')) # 添加左右 符号
+# print(','.join(['1', '2']))
+# print('Alex'.lower()) #小写
+# print(*'Alex'.upper()) # 大写
+print('\nAlex'.lstrip()) # 去除左边的空格
+print('alex\n'.rstrip()) # 去除右边边的空格
+print('\nAlex\n'.strip()) # 去除全部的\n 
