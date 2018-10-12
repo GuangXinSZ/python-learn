25b8685b2d2c517e646ec8b21345ff60919e7c53
AnimationXin <18379177354@163.com>
2018-10-13 00:53:38 +0800

    feature: 装饰器

--- day4/decorator.py
+++ day4/decorator.py
@@ -1,5 +1,15 @@
 import time
 
+# 装饰器
+def timer(func):n 
+  def wrapper(*args, **kwargs):
+    start_time = time.time()
+    func()
+    stop_time = time.time()
+    print('the func run time is %s' %(stop_time-start_time))
+  return wrapper
+
+@timer
 def test1():
   time.sleep(3)
   print('in the test')
