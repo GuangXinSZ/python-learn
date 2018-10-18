c34aa5b938d64417ad54f31498c6f7675d631c2a
liugx <18379177354@163.com>
2018-10-18 16:24:52 +0800

    feature: 装饰器。

--- day4/decorator_1.py
+++ day4/decorator_1.py
@@ -1,10 +1,13 @@
 import time
 
-def deco(func):
-  start_time = time.time()
-  func()
-  stop_time = time.time()
-  print('the func run time is %s' %(stop_time-start_time))
+def timer(func):
+  def deco():
+    start_time = time.time()
+    func()
+    stop_time = time.time()
+    print('the func run time is %s' %(stop_time-start_time))
+  
+  return deco
 
 def test1():
   time.sleep(3)
@@ -14,3 +17,6 @@ def test2():
   time.sleep(3)
   print('test2')
 
+temp1 = timer(test1)
+temp1()
+temp2 = timer(test2)
