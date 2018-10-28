54ba610ae7497ec8cfc4945d910f08e6437df0a2
liugx <18379177354@163.com>
2018-10-28 22:40:00 +0800

    feature: filter

--- day4/内置方法.py
+++ day4/内置方法.py
@@ -3,5 +3,9 @@ import time
 b = bytearray("abcde", encoding="utf-8")
 # print(all([1, -5, 2]))
 # print(any([0, 1]))
-b[0] = 98
-print(b)
+# b[0] = 98
+# filter过滤您想要的数据 lambda函数
+
+res = filter(lambda n: n > 5, range(10))
+for i in res:
+  print(i)
