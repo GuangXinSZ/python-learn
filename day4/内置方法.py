2f4bd173d0b50ca24aeae1cf8446d448ccdbcbff
liugx <18379177354@163.com>
2018-10-29 00:00:34 +0800

    内置方法

--- day4/内置方法.py
+++ day4/内置方法.py
@@ -5,7 +5,16 @@ b = bytearray("abcde", encoding="utf-8")
 # print(any([0, 1]))
 # b[0] = 98
 # filter过滤您想要的数据 lambda函数
-
+# sorted 排序 根据item or  key = lambda x: x[1]
 res = filter(lambda n: n > 5, range(10))
-for i in res:
+
+# def test():
+#   local_var = 333
+#   print(locals())
+# test()
+
+a = [1, 2, 3, 4]
+b = ['a', 'b', 'c', 'd']
+# zip合并两数组
+for i in zip(a, b):
   print(i)
