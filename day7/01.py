16f7f101c5fca271932a4c45a3484d70a9c0f28b
GuangXIn <18379177354@163.com>
2019-01-22 23:17:34 +0800

    feature: 生成器。

---
+++ day7/01.py
@@ -0,0 +1,16 @@
+import time
+
+def timmer(func):
+  def wrapper(*args, **kwargs):
+    start_time = time.time()
+    func()
+    stop_time = time.time()
+    print('the fun run time is %s' %(start_time - stop_time))
+  return wrapper
+
+@timmer
+def test1():
+  time.sleep(3)
+  print('123')
+
+test1()
